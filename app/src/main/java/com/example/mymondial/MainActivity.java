398fc34fe27674d7952cd4d423135c57c16ecea4
HAALAND <jordan.alvesdasilva@edu.esiee.fr>
2022-10-24 12:04:51 +0200

    Page d'acceuil en cours

--- app/src/main/java/com/example/mymondial/MainActivity.java
+++ app/src/main/java/com/example/mymondial/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mymondial;
 
+import android.content.Context;
 import android.os.Bundle;
-
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -9,25 +11,18 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.mymondial.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,28 +32,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-
-    // Permet de pouvoir récupérer su l'API des informations (ici nom des joueurs allemand)
+        // REQUEST API FOR MATCH
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://app.sportdataapi.com/api/v1/soccer/players?apikey=193beda0-5093-11ed-aa03-b339e6eb1617&country_id=48";
-
-        // Request a string response from the provided URL.
+        ArrayList<String> live = new ArrayList<String>();
+        ListView text = (ListView) findViewById(R.id.match_live);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, live );
+        String url = "https://app.sportdataapi.com/api/v1/soccer/matches?apikey=193beda0-5093-11ed-aa03-b339e6eb1617&season_id=1243";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        System.out.println("Response is: " + response.substring(0,500));
                         try {
                             JSONObject jObject = new JSONObject(response);
                             JSONArray data = jObject.getJSONArray("data");
+                            System.out.println("Response is: " + response.substring(0,500));
                             for (int i = 0; i < data.length(); i++) {
-                                String firstname = data.getJSONObject(i).getString("firstname");
-                                String lastname = data.getJSONObject(i).getString("lastname");
-                                System.out.println(firstname +" " + lastname);
+                                String home_team_name = data.getJSONObject(i).getJSONObject("home_team").getString("name");
+                                String away_team_name = data.getJSONObject(i).getJSONObject("away_team").getString("name");
+                                String score = data.getJSONObject(i).getJSONObject("stats").getString("ft_score");
+                                live.add(home_team_name + "  " + score + "  " + away_team_name );
                             }
-
+                            text.setAdapter(arrayAdapter);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -67,60 +63,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 System.out.println("That didn't work!");
+                System.out.println(error);
             }
         });
 
-        // Add the request to the RequestQueue.
+// Add the request to the RequestQueue.
         queue.add(stringRequest);
+        System.out.println(live);
 
-    // Fin de la récupération
-
-
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
